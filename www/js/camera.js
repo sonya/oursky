20787d3dc27a43d2681058e5e86a9443346388cb
Suguru Murakami <sugumura@gmail.com>
2015-04-12 02:51:27 +0900

    add launch camera

--- www/js/camera.js
+++ www/js/camera.js
@@ -3,10 +3,39 @@
 
     var app = angular.module('oursky');
 
-    app.controller('CameraController', function($scope) {
+    app.controller('CameraController', ['$scope', function($scope) {
 
         console.log('CameraController');
-    });
+
+        $scope.launch = function() {
+            console.log('CameraController / launch method');
+
+            // launch camera
+            navigator.camera.getPicture (
+                onSuccess,
+                onFail,
+                { quality: 50, destinationType: Camera.DestinationType.DATA_URL}
+            );
+
+
+            /**
+             * call success
+             * @param imageData
+             */
+            function onSuccess (imageData) {
+                var image = document.getElementById ('picture');
+                image.src = "data:image/jpeg;base64," + imageData;
+            }
+
+            /**
+             * call fail
+             * @param message
+             */
+            function onFail (message) {
+                alert ('error!: ' + message);
+            }
+        };
+    }]);
 
 
 })();
