3427af1c4b7e83028a1094f6ddc8ed89bf7117b1
sonya <sonya.huang@gmail.com>
2015-04-11 23:56:02 -0400

    refactor fake data to simulate actual
    process of retrieving photo list from an external source. added more seed
    photos.

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -5,48 +5,46 @@
 
     app.controller('CalendarController', function($scope) {
 
+        var imageList = {};
+        var getImageList = function() {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    imageList = JSON.parse(xhr.responseText);
+                    getPhotoCellsForCurrentMonth();
+                    $scope.$apply();
+                }
+            };
+            xhr.open('GET', 'img/seeddata/seeddata.json', true);
+            xhr.send();
+        };
+
         // eventually this will come from a photo service
         var getImageDataForDate = function(date) {
-            // colors: deep blue blue, light blue, pale blue, milky, other
-            var fakeImageData = [
-                {
-                    url: 'img/IMG_0197_small.JPG',
-                    time: '10:00 AM',
-                    location: 'Somerville, MA',
-                    color: 'Deep blue'
-                },
-                {
-                    url: 'img/IMG_0201_small.JPG',
-                    time: '11:00 AM',
-                    location: 'Somerville, MA',
-                    color: 'Milky'
-                },
-                {
-                    url: 'img/IMG_0202_small.JPG',
-                    time: '4:00 PM',
-                    location: 'Somerville, MA',
-                    color: 'Milky'
-                },
-                {
-                    url: 'img/IMG_0208_small.JPG',
-                    time: '5:00 PM',
-                    location: 'Somerville, MA',
-                    color: 'Deep blue'
-                }
-            ];
+            var year = date.getYear() + 1900;
+            var month = date.getMonth() + 1;
+            if (month < 10) {
+                month = '0' + month;
+            }
+            var datedate = date.getDate();
+            if (datedate < 10) {
+                datedate = '0' + datedate;
+            }
 
-            // each day may have zero or one or many images
-            // we are just kind of simulating that
-            var dateNumber = date.getDate();
-            var numberOfImages = (dateNumber * dateNumber - (dateNumber % 4) + 1) % 3;
+            var dateKey = year + '-' + month + '-' + datedate;
 
             var images = [];
-            for (var i = 0; i < numberOfImages; i++) {
-                var random = Math.floor(Math.random() * fakeImageData.length);
-                images.push(fakeImageData[random]);
+
+            if (dateKey in imageList) {
+                images = imageList[dateKey];
             }
 
             return images;
+        };
+
+        var getFirstImageForDate = function(dateObject) {
+            var images = getImageDataForDate(dateObject);
+            return (images.length > 0) ? images[0].url : null;
         };
 
         var getPhotoCellsForCurrentMonth = function () {
@@ -69,11 +67,14 @@
             if (dayOfWeek > 0) {
                 var pastDate = new Date(year, month, 0).getDate();
                 for (var i = 0; i < dayOfWeek; i++) {
+                    var dateFromLastMonth = pastDate - dayOfWeek + i + 1;
+                    var dateObjectFromLastMonth = new Date(year, month-1, dateFromLastMonth);
                     // push empty cells until the first day of the month
                     currentRow.push({
-                        date: pastDate - dayOfWeek + i + 1,
-                        dateObject: null,
-                        imageURI: null
+                        date: dateFromLastMonth,
+                        dateObject: dateObjectFromLastMonth,
+                        imageURI: getFirstImageForDate(dateObjectFromLastMonth),
+                        cssClass: 'past-month'
                     });
                 }
             }
@@ -87,16 +88,10 @@
                     currentRow = [];
                 }
 
-                var images = getImageDataForDate(dateObject);
-                var imageURI = null;
-                if (images.length > 0) {
-                    imageURI = images[0].url;
-                }
-
                 currentRow.push({
                     date: i,
                     dateObject: dateObject,
-                    imageURI: imageURI
+                    imageURI: getFirstImageForDate(dateObject)
                 });
             }
 
@@ -107,7 +102,8 @@
                     currentRow.push({
                         date: nextDate++,
                         dateObject: null,
-                        imageURI: null
+                        imageURI: null,
+                        cssClass: 'next-month'
                     });
                 }
 
@@ -169,7 +165,7 @@
             hideNode('calendar-day');
         };
 
-        getPhotoCellsForCurrentMonth();
+        getImageList();
 
     });
 
