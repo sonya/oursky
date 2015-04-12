1d67695ef4c47005abbe18dcd2cf752926adfdc5
sonya <sonya.huang@gmail.com>
2015-04-11 20:22:47 -0400

    add calendar day view
    remove some extra elements from index.html that were clogging up the DOM
    inspector. add fake data for the calendar view.

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -5,10 +5,55 @@
 
     app.controller('CalendarController', function($scope) {
 
+        // eventually this will come from a photo service
+        var getImageDataForDate = function(date) {
+            // colors: deep blue blue, light blue, pale blue, milky, other
+            var fakeImageData = [
+                {
+                    url: 'img/IMG_0197_small.JPG',
+                    time: '10:00 AM',
+                    location: 'Somerville, MA',
+                    color: 'Deep blue'
+                },
+                {
+                    url: 'img/IMG_0201_small.JPG',
+                    time: '11:00 AM',
+                    location: 'Somerville, MA',
+                    color: 'Milky'
+                },
+                {
+                    url: 'img/IMG_0202_small.JPG',
+                    time: '4:00 PM',
+                    location: 'Somerville, MA',
+                    color: 'Milky'
+                },
+                {
+                    url: 'img/IMG_0208_small.JPG',
+                    time: '5:00 PM',
+                    location: 'Somerville, MA',
+                    color: 'Deep blue'
+                }
+            ];
+
+            // each day may have zero or one or many images
+            // we are just kind of simulating that
+            var dateNumber = date.getDate();
+            var numberOfImages = (dateNumber * dateNumber - (dateNumber % 4) + 1) % 3;
+
+            var images = [];
+            for (var i = 0; i < numberOfImages; i++) {
+                var random = Math.floor(Math.random() * fakeImageData.length);
+                images.push(fakeImageData[random]);
+            }
+
+            return images;
+        };
+
         var getPhotoCellsForCurrentMonth = function () {
             var rows = [];
 
             var date = new Date();
+
             var month = date.getMonth() + 1;
             var year = date.getYear();
 
@@ -25,7 +70,7 @@
                 currentRow.push({
                     // TODO: get date from previous month
                     date: 0,
-                    month: month,
+                    dateObject: null,
                     imageURI: null
                 });
             }
@@ -39,11 +84,15 @@
                     currentRow = [];
                 }
 
-                var imageURI = 'img/IMG_0197_small.JPG';
+                var images = getImageDataForDate(date);
+                var imageURI = null;
+                if (images.length > 0) {
+                    imageURI = images[0].url;
+                }
 
                 currentRow.push({
                     date: i,
-                    month: month,
+                    dateObject: date,
                     imageURI: imageURI
                 });
             }
@@ -54,7 +103,7 @@
                     currentRow.push({
                         // TODO: add dates for next month
                         date: 0,
-                        month: month,
+                        dateObject: null,
                         imageURI: null
                     });
                 }
@@ -69,28 +118,49 @@
 
             $scope.currentMonth = englishMonthNames[month-1];
             $scope.calendarRows = rows;
+        };
 
-            console.log($scope.calendarRows);
+        // i don't know if there's an easier way to do this in onsen
+        var showNode = function(domId) {
+            var domNode = document.getElementById(domId);
+            domNode.className = domNode.className.replace( /(?:^|\s)hidden(?!\S)/g , '' );
+        };
+
+        var hideNode = function(domId) {
+            var domNode = document.getElementById(domId);
+            if (domNode.className.indexOf('hidden') == -1) {
+                domNode.className += ' hidden';
+            }
         };
 
         $scope.calendarItemClick = function(scope) {
-            var date = scope.calendarCell.date;
-            var images = [];
-
-            images.push({
-                location: 'Somerville, MA'
-                time: '5:00 pm',
-                color: 'Blue',
-                url: 'img/IMG_0208_small.JPG'
-            });
-            images.push({
-                location: 'Somerville, MA'
-                time: '9:00 am',
-                color: 'Blue',
-                url: 'img/IMG_0197_small.JPG'
-            });
+            var date = scope.calendarCell.dateObject;
+            var images = getImageDataForDate(date);
 
             $scope.images = images;
+
+            // i am sure there is a faster way to do this with onsen
+            var listInnerHTML = '';
+            for (var i = 0; i < images.length; i++) {
+                var imageItem = images[i];
+                listInnerHTML += '\
+                <ons-list-item class="day-item" modifier="tappable">\
+                    <div class="day-image"><img src="' + imageItem.url + '" /></div>\
+                    <div class="day-caption day-location">Location: ' + imageItem.location + '</div>\
+                    <div class="day-caption day-time">Time: ' + imageItem.time + '</div>\
+                    <div class="day-caption day-color">Color: ' + imageItem.color + '</div>\
+                </ons-list-item>';
+            }
+            var list = document.getElementById('calendar-day-list');
+            list.innerHTML = listInnerHTML;
+
+            showNode('calendar-day');
+            hideNode('calendar');
+        };
+
+        $scope.backToCalendar = function(scope) {
+            showNode('calendar');
+            hideNode('calendar-day');
         };
 
         getPhotoCellsForCurrentMonth();
